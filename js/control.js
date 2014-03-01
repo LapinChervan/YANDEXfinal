c9a4abd230130f80f1703db5cf3584efeb1494b8
lapinlapin <invazia@mail.ru>
2014-03-01 17:16:22 +0200

    Заготовка (Конструктор)

--- js/control.js
+++ js/control.js
@@ -42,6 +42,56 @@ CONTROL.dom = (function() {
 
 })();
 
-CONTROL.user = (function() {
-  
-})();
+CONTROL.User = function(name, password) {
+	this.name = name;	// логин
+	this.password = password; // пароль
+	this.categories = { // объект с категориями
+		accounts: [],  // категории счетов
+		costs: [],  // категории расходов
+		gain: [] // категории доходов
+	};
+	this.mainCurr = 'UAH'; // основная валюта
+	this.currency = {  // объект с валютой, заголовками и курсом
+		uah: {			
+			title: 'Грн',
+			price: 0.00
+		},
+		rub: {  
+			title: 'Руб',
+			price: 0.00
+		},
+		usd: {			
+			title: 'Долл',
+			price: 0.00
+		},
+		eur: {
+			title: 'Евр',
+			price: 0.00
+		}
+	};
+	this.history = []; // история операций
+	this.total = { 	  // итоги по доходам и расходам
+		costs: {
+			thisMonth: 0,
+			priorMont: 0,
+			thisYear: 0,
+			priorYear: 0
+		},
+		gain: {
+			thisMonth: 0,
+			priorMont: 0,
+			thisYear: 0,
+			priorYear: 0
+		}
+	};
+}
+
+CONTROL.User.prototype.newCategory = function(type, category) {
+	if (this.categories[type] && category) {
+		this.categories[type].push(category);
+	}
+}
+
+CONTROL.User.prototype.removeCategory = function(type, category) {
+
+}
