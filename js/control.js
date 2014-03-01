0bb8f5e126b2d3474d2d62c5d9ad5a3394e3280f
lapinlapin <invazia@mail.ru>
2014-03-01 20:13:36 +0200

    Полифилы

--- js/control.js
+++ js/control.js
@@ -1,39 +1,5 @@
 var CONTROL = {};
 
-CONTROL.initialize = (function() {
-
-	var events = {};
-
-	if (typeof window.addEventListener === 'function') {
-		events.addListener = function(elem, type, handler) {
-			elem.addEventListener(type, handler, false);
-		};
-
-		events.removeListener = function(elem, type, handler) {
-			elem.removeEventListener(type, handler, false);
-		};
-	} else if (typeof document.attachEvent === 'function') {
-		events.addListener = function(elem, type, handler) {
-			elem.attachEvent('on' + type, handler);
-		}
-
-		events.removeListener = function(elem, type, handler) {
-			elem.detachEvent('on' + type, handler);
-		}
-	} else {
-		events.addListener = function(elem, type, handler) {
-			elem['on' + type] = handler;
-		}
-
-		events.removeListener = function(elem, type, handler) {
-			elem['on' + type] = null;
-		}
-	}
-
-	return events;
-
-})();
-
 CONTROL.animate = (function() {
 
 })();
@@ -43,15 +9,15 @@ CONTROL.dom = (function() {
 })();
 
 CONTROL.User = function(name, password) {
-	this.name = name;	// логин
-	this.password = password; // пароль
-	this.categories = { // объект с категориями
-		accounts: [],  // категории счетов
-		costs: [],  // категории расходов
-		gain: [] // категории доходов
+	this.name = name;	
+	this.password = password;
+	this.categories = { 
+		accounts: [], 
+		costs: [],  
+		gain: [] 
 	};
-	this.mainCurr = 'UAH'; // основная валюта
-	this.currency = {  // объект с валютой, заголовками и курсом
+	this.mainCurr = 'Грн';
+	this.currency = { 
 		uah: {			
 			title: 'Грн',
 			price: 0.00
@@ -69,19 +35,19 @@ CONTROL.User = function(name, password) {
 			price: 0.00
 		}
 	};
-	this.history = []; // история операций
-	this.total = { 	  // итоги по доходам и расходам
+	this.history = [];
+	this.total = { 	  
 		costs: {
-			thisMonth: 0,
-			priorMont: 0,
-			thisYear: 0,
-			priorYear: 0
+			thisMonth: 0 + this.mainCurr,
+			priorMont: 0 + this.mainCurr,
+			thisYear: 0 + this.mainCurr,
+			priorYear: 0 + this.mainCurr
 		},
 		gain: {
-			thisMonth: 0,
-			priorMont: 0,
-			thisYear: 0,
-			priorYear: 0
+			thisMonth: 0 + this.mainCurr,
+			priorMont: 0 + this.mainCurr,
+			thisYear: 0 + this.mainCurr,
+			priorYear: 0 + this.mainCurr
 		}
 	};
 }
@@ -95,3 +61,6 @@ CONTROL.User.prototype.newCategory = function(type, category) {
 CONTROL.User.prototype.removeCategory = function(type, category) {
 
 }
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,16 +1,15 @@
 var	doc = document, // документ
-	events = CONTROL.initialize, // инициализация
 	buttonReg = doc.getElementsByClassName('header__reg')[0], // кнопка регистрации
 	buttonAuth = doc.getElementsByClassName('header__auth')[0], // кнопка авторизации
 	formReg = doc.getElementById('form-reg').innerHTML, // форма регистрации
 	formLogin = doc.getElementById('form-login').innerHTML; // форма авторизации
 
 // вызов формы регистрации
-events.addListener(buttonReg, 'click', function() {
+buttonReg.addEventListener('click', function() {
 	doc.body.innerHTML = formReg;
 }, false);
 
 // вызов формы авторизации
-events.addListener(buttonAuth, 'click', function() {
+buttonAuth.addEventListener('click', function() {
 	doc.body.innerHTML = formLogin;
 }, false);
