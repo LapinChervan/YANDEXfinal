b2a93b8b86e14553f7a2ede4fdd2d3aac541220a
lapinlapin <invazia@mail.ru>
2014-03-01 13:44:53 +0200

    Инициализация обработчиков, обработчики(авт,рег)

---
+++ js/control.js
@@ -0,0 +1,46 @@
+var CONTROL = {};
+
+CONTROL.initialize = (function() {
+	
+	var events = {};
+
+	if (typeof window.addEventListener === 'function') {
+		events.addListener = function(elem, type, handler) {
+			elem.addEventListener(type, handler, false);
+		};
+
+		events.removeListener = function(elem, type, handler) {
+			elem.removeEventListener(type, handler, false);
+		};
+	} else if (typeof document.attachEvent === 'function') {
+		events.addListener = function(elem, type, handler) {
+			elem.attachEvent('on' + type, handler);
+		}
+
+		events.removeListener = function(elem, type, handler) {
+			elem.detachEvent('on' + type, handler);
+		}
+	} else {
+		events.addListener = function(elem, type, handler) {
+			elem['on' + type] = handler;
+		}
+
+		events.removeListener = function(elem, type, handler) {
+			elem['on' + type] = null;
+		}
+	}
+
+	return events;
+
+})();
+
+
+CONTROL.dom = (function() {
+
+})();
+
+
+
+CONTROL.user = (function() {
+  
+})();
---
+++ js/main.js
@@ -0,0 +1,18 @@
+var	doc = document, // документ
+	events = CONTROL.initialize, // инициализация
+	buttonReg = doc.getElementsByClassName('header__reg')[0], // кнопка регистрации
+	buttonAuth = doc.getElementsByClassName('header__auth')[0], // кнопка авторизации
+	formReg = doc.getElementById('form-reg').innerHTML, // форма регистрации
+	formLogin = doc.getElementById('form-login').innerHTML; // форма авторизации
+
+// вызов формы регистрации
+events.addListener(buttonReg, 'click', function() {
+	doc.body.innerHTML = formReg;
+}, false);
+
+// вызов формы авторизации
+events.addListener(buttonAuth, 'click', function() {
+	doc.body.innerHTML = formLogin;
+}, false);
+
+
