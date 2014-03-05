7c0ac3d08f28c21d4ec0f3a31106cad91282103a
lapinlapin <invazia@mail.ru>
2014-03-05 22:57:03 +0200

    зависимость фикс

--- js/control.js
+++ js/control.js
@@ -6,7 +6,7 @@ CONTROL.ajax = (function() {
 	function toServer(link, callback) {
 		var xhr = new XMLHttpRequest(),
 			data;
-    
+
         xhr.open('GET', link); 
         xhr.onreadystatechange = function() {
             if (xhr.readyState != 4) return; 
@@ -23,22 +23,23 @@ CONTROL.ajax = (function() {
 	}
 })();
 
-CONTROL.access = (function() {	
+CONTROL.access = (function() {
+    var ajax = CONTROL.ajax;
 	function showContent() {
 		var doc = document;
-		doc.body.innerHTML = doc.getElementById('user-form').innerHTML; 
+		    doc.body.innerHTML = doc.getElementById('user-form').innerHTML;
 	}
 
     //TODO отсылать логин и пароль не GETом
 	function registration(user, password) {
 		if (user && password) {
-			CONTROL.ajax.toServer('http://localhost:1111/reg?login=' + user +'&password='+ password);
+            ajax.toServer('http://localhost:1111/reg?login=' + user +'&password='+ password);
 		}
 	}
 
     function authorization(user, password) {
         if (user && password) {
-            CONTROL.ajax.toServer('http://localhost:1111/auth?login=' + user +'&password='+ password, showContent);
+            ajax.toServer('http://localhost:1111/auth?login=' + user +'&password='+ password, showContent);
         }
     }
 
