a44397d3ffab3fd223e68076811d19ed42f9fa1f
lapinlapin <invazia@mail.ru>
2014-03-08 16:47:36 +0200

    Объединение слоя

--- js/control.js
+++ js/control.js
@@ -71,6 +71,10 @@ CONTROL.access = (function() {
         });
 
 
+        doc.getElementsByClassName('first__ul__li__button')[0].addEventListener('click', function() {
+            CONTROL.layer.createLayer({content: doc.getElementById('form__plus').innerHTML});
+        }, false);
+
         doc.getElementsByClassName('add_cat_plus')[0].addEventListener('click', function(e) {
             e.preventDefault();
             ajax.toServer('http://localhost:1111/newCategories?login='+ window.login +'&plus=' + doc.getElementsByClassName('edit_cat_plus')[0].value);
@@ -106,6 +110,61 @@ CONTROL.access = (function() {
 		registration: registration,
         authorization: authorization
 	}
+})();
+
+CONTROL.layer = (function () {
+    function createLayer (options) {
+        var modal = this.modal = document.createElement('div'),
+            layer = this.layer = document.createElement('div'),
+            optionsObject, key, parent;
+        optionsObject = getDefaultOptions();
+        for (key in options) {
+            if (!options.hasOwnProperty(key)) continue;
+            optionsObject[key] = options[key];
+        }
+        this.parent = parent = optionsObject.parent;
+        modal.className = optionsObject.clsOpacityLayer;
+        layer.className = optionsObject.clsContentLayer;
+        layer.innerHTML = optionsObject.content;
+        parent.appendChild(modal);
+        parent.appendChild(layer);
+        layer.addEventListener('click', function (e) {
+            var event = e || window.event;
+            event.stopPropagation();
+        });
+    }
+
+    function getDefaultOptions() {
+        return {
+            parent: document.body,
+            clsOpacityLayer: 'modal',
+            clsContentLayer: 'layer',
+            content: undefined
+        }
+    }
+
+    function destroyLayer() {
+        var layer = this.layer,
+            modal = this.modal,
+            parent = this.parent;
+        if (!modal || !layer) {
+            return;
+        }
+        parent.removeChild(modal);
+        parent.removeChild(layer);
+        this.parent = undefined;
+        this.layer = undefined;
+        this.modal = undefined;
+    }
+
+    document.addEventListener('click', function(){
+        destroyLayer.call(CONTROL.layer)
+    });
+
+    return {
+        createLayer: createLayer,
+        destroyLayer: destroyLayer
+    }
 })();
 
 
