6f2ff15083a0071e9b7d1284aa49b340960e851a
lapinlapin <invazia@mail.ru>
2014-03-10 13:37:56 +0200

    fix

--- js/server.js
+++ js/server.js
@@ -7,7 +7,6 @@ http.createServer(function(req, res) {
 	var data = url.parse(req.url, true);
 
 	if (data.pathname === '/reg') {
-
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
 		requests.reg(data.query.login, data.query.password);
 
@@ -32,8 +31,5 @@ http.createServer(function(req, res) {
     if (data.pathname === '/newCategories') {
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
         requests.newCat(data.query.login, data.query.typ, data.query.cat, res);
-     //   res.end();
     }
-	
 }).listen(1111);
-
