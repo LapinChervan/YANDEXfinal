e726cdabb0c2863ad1cee8ae9c0ce1188f7d12b6
DimaChervan <dmitriy.chervan@yandex.ru>
2014-03-13 22:29:16 +0200

    перенес в объект

--- js/server.js
+++ js/server.js
@@ -6,66 +6,56 @@ var http = require('http'),
 http.createServer(function(req, res) {
 	var data = url.parse(req.url, true);
 
-	if (data.pathname === '/reg') {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-		requests.reg(data.query.login, data.query.password);
-
-		res.end();
-	}
-
-    if (data.pathname === '/auth') {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        requests.auth(data.query.login, data.query.password, res);
-    }
-
-    if (data.pathname === '/currency') {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        if (data.query.curr) {
-            requests.changeCurr(data.query.login, data.query.curr, res);
+    var pathObj = {
+        '/reg': function() {
+            requests.reg(data.query.login, data.query.password);
+        },
+        '/auth': function() {
+            requests.auth(data.query.login, data.query.password, res);
+        },
+        '/currency': function() {
+            if (data.query.curr) {
+                requests.changeCurr(data.query.login, data.query.curr, res);
+            }
+            else {
+                requests.setMainCurr(data.query.login, data.query.valuta, res);
+            }
+        },
+        '/newCategories': function() {
+            requests.newCat(data.query.login, data.query.typ, data.query.cat, res);
+        },
+        '/historyNewOper': function() {
+            var obj = {
+                date: data.query.date,
+                sch: data.query.sch,
+                cat: data.query.cat,
+                sum: data.query.sum,
+                comm: data.query.comment,
+                type: data.query.type,
+                id: data.query.id
+            };
+            requests.newOper(data.query.login, data.query.type, obj, res);
+        },
+        '/historyRemove': function() {
+            requests.removeOper(data.query.login, data.query.id, res);
+        },
+        '/renameCategory': function() {
+            var obj = {
+                login: data.query.login,
+                type: data.query.type,
+                old: data.query.old,
+                new: data.query.new
+            };
+            requests.renameCat(obj, res);
+        },
+        '/removeCategory': function() {
+            requests.removeCat(data.query.login, data.query.type, data.query.old, res);
         }
-        else {
-            requests.setMainCurr(data.query.login, data.query.valuta, res);
-        }
-    }
+    };
 
-    if (data.pathname === '/newCategories') {
+    if (pathObj[data.pathname]) {
         res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        requests.newCat(data.query.login, data.query.typ, data.query.cat, res);
-    }
-
-    if (data.pathname === '/historyNewOper') {
-        var obj = {
-            date: data.query.date,
-            sch: data.query.sch,
-            cat: data.query.cat,
-            sum: data.query.sum,
-            comm: data.query.comment,
-            type: data.query.type,
-            id: data.query.id
-        };
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        requests.newOper(data.query.login, data.query.type, obj, res);
-    }
-
-    if (data.pathname === '/historyRemove') {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        requests.removeOper(data.query.login, data.query.id, res);
-    }
-
-    if (data.pathname === '/renameCategory') {
-        var obj = {
-            login: data.query.login,
-            type: data.query.type,
-            old: data.query.old,
-            new: data.query.new
-        }
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        requests.renameCat(obj, res);
-    }
-
-    if (data.pathname === '/removeCategory') {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        requests.removeCat(data.query.login, data.query.type, data.query.old, res);
+        pathObj[data.pathname]();
     }
 
 }).listen(1111);
