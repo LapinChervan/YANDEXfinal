b4c5a8a7b78b4e0ef15dc3af5430a6ce42f7cba6
lapinlapin <invazia@mail.ru>
2014-03-22 22:26:25 +0200

    ПЕРЕХОД НА CONNECT (СТАТИЧЕСКИЙ СЕРВЕР)

--- js/server.js
+++ js/server.js
@@ -6,8 +6,8 @@ var http = require('http'),
 
 var app = connect()
     .use(connect.cookieParser())
-    .use(function(req,res,next){
-        var data = url.parse(req.url, true),
+    .use(function(req, res, next){
+        var data = url.parse(req.url, true);
             pathObj = {
                 '/reg': function() {
                     requests.reg(data.query.login, data.query.password);
@@ -63,68 +63,9 @@ var app = connect()
         if (pathObj[data.pathname]) {
             res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
             pathObj[data.pathname]();
+        } else {
+            next();
         }
-        next();
-    })
-    .use(connect.static('/Users/1/Documents/GitHub/yandex'));
+    });
+    app.use(connect.static('/Users/nvzC/Documents/GitHub/yandex'));
 http.createServer(app).listen(1111);
-/*http.createServer(function(req, res) {
-	var data = url.parse(req.url, true),
-        pathObj = {
-            '/reg': function() {
-                requests.reg(data.query.login, data.query.password);
-            },
-
-            '/auth': function() {
-                requests.auth(data.query.login, data.query.password, data.query.start, data.query.end, res);
-            },
-
-            '/currency': function() {
-                if (data.query.curr) {
-                    requests.changeCurr(data.query.login, data.query.curr, data.query.price, res);
-                }
-                else {
-                    requests.setMainCurr(data.query.login, data.query.valuta, res);
-                }
-            },
-
-            '/newCategories': function() {
-                requests.newCat(data.query.login, data.query.typ, data.query.cat, res);
-            },
-
-            '/historyNewOper': function() {
-                requests.newOper(data.query.login, data.query.type, data.query.formData, res);
-            },
-
-            '/historyRemove': function() {
-                requests.removeOper(data.query.login, data.query.type, data.query.id, res);
-            },
-
-            '/renameCategory': function() {
-                var obj = {
-                    login: data.query.login,
-                    type: data.query.type,
-                    old: data.query.old,
-                    new: data.query.new
-                };
-                requests.renameCat(obj, res);
-            },
-            '/removeCategory': function() {
-                requests.removeCat(data.query.login, data.query.type, data.query.old, res);
-            },
-            '/findOperation': function() {
-                if (!data.query.account) {
-                    requests.dateFilter(data.query.login, data.query.start, data.query.end, res);
-                }
-                else {
-                    requests.historyFilter(data.query.login, data.query.account, res, data.query.type, data.query.start, data.query.end);
-                }
-            }
-        };
-
-    if (pathObj[data.pathname]) {
-        res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
-        pathObj[data.pathname]();
-    }
-
-}).listen(1111);*/
