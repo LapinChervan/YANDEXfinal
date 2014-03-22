2962f9af507dd2759a618b904a23183d6be541f9
DimaChervan <dmitriy.chervan@yandex.ru>
2014-03-22 21:20:58 +0200

    connect

--- js/server.js
+++ js/server.js
@@ -1,9 +1,74 @@
 var http = require('http'),
 	url = require('url'),
-	util = require('util');
+	util = require('util'),
+    connect = require('connect'),
 	requests = require('requests');
-	
-http.createServer(function(req, res) {
+
+var app = connect()
+    .use(connect.cookieParser())
+    .use(function(req,res,next){
+        var data = url.parse(req.url, true),
+            pathObj = {
+                '/reg': function() {
+                    requests.reg(data.query.login, data.query.password);
+                },
+
+                '/auth': function() {
+                    requests.auth(data.query.login, data.query.password, data.query.start, data.query.end, res);
+                },
+
+                '/currency': function() {
+                    if (data.query.curr) {
+                        requests.changeCurr(data.query.login, data.query.curr, data.query.price, res);
+                    }
+                    else {
+                        requests.setMainCurr(data.query.login, data.query.valuta, res);
+                    }
+                },
+
+                '/newCategories': function() {
+                    requests.newCat(data.query.login, data.query.typ, data.query.cat, res);
+                },
+
+                '/historyNewOper': function() {
+                    requests.newOper(data.query.login, data.query.type, data.query.formData, res);
+                },
+
+                '/historyRemove': function() {
+                    requests.removeOper(data.query.login, data.query.type, data.query.id, res);
+                },
+
+                '/renameCategory': function() {
+                    var obj = {
+                        login: data.query.login,
+                        type: data.query.type,
+                        old: data.query.old,
+                        new: data.query.new
+                    };
+                    requests.renameCat(obj, res);
+                },
+                '/removeCategory': function() {
+                    requests.removeCat(data.query.login, data.query.type, data.query.old, res);
+                },
+                '/findOperation': function() {
+                    if (!data.query.account) {
+                        requests.dateFilter(data.query.login, data.query.start, data.query.end, res);
+                    }
+                    else {
+                        requests.historyFilter(data.query.login, data.query.account, res, data.query.type, data.query.start, data.query.end);
+                    }
+                }
+            };
+
+        if (pathObj[data.pathname]) {
+            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8', 'Access-Control-Allow-Origin': '*'});
+            pathObj[data.pathname]();
+        }
+        next();
+    })
+    .use(connect.static('/Users/1/Documents/GitHub/yandex'));
+http.createServer(app).listen(1111);
+/*http.createServer(function(req, res) {
 	var data = url.parse(req.url, true),
         pathObj = {
             '/reg': function() {
@@ -62,4 +127,4 @@ http.createServer(function(req, res) {
         pathObj[data.pathname]();
     }
 
-}).listen(1111);
+}).listen(1111);*/
