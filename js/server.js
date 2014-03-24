5133aad7b098368d1499cb36340af8cb0de2ea1d
lapinlapin <invazia@mail.ru>
2014-03-24 23:26:35 +0200

    Поправки (Сервер)

--- js/server.js
+++ js/server.js
@@ -16,7 +16,6 @@ var http = require('http'),
                         requests.setMainCurr(data.query.login, data.query.valuta, res);
                     }
                 },
-
                 '/newCategories': function() {
                     requests.newCat(data.query.login, data.query.typ, data.query.cat, res);
                 },
@@ -60,31 +59,18 @@ var http = require('http'),
     });
 
     app.use(function(req, res, next) {
-        // if (req.headers.origin === 'http://habrahabr.ru') {
         var data = url.parse(req.url, true);
         if (data.pathname === '/auth') {
-        //    var crypto = require('crypto');
-         //   console.log(crypto.createHash('md5').update(data.query.login + data.query.password + Date.parse(new Date())).digest("hex"));
-        //    var date = new Date();
-          //  date.setTime(date.getTime()+(7*24*60*60*1000));
-          //  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8','Set-Cookie':'zalupa='+crypto.createHash('md5').update(data.query.login + data.query.password + Date.parse(new Date())).digest("hex")+';path=/;expires='+date.toGMTString()+';HttpOnly'});
             requests.auth(data.query.login, data.query.password, data.query.start, data.query.end, res);
         }
-        //   }
         else {
             next();
         }
     });
 
     app.use(function(req, res, next) {
-        // if (req.headers.origin === 'http://habrahabr.ru') {
         var data = url.parse(req.url, true);
         if (data.pathname === '/cookie') {
-            //    var crypto = require('crypto');
-            //   console.log(crypto.createHash('md5').update(data.query.login + data.query.password + Date.parse(new Date())).digest("hex"));
-            //    var date = new Date();
-            //  date.setTime(date.getTime()+(7*24*60*60*1000));
-            //  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8','Set-Cookie':'zalupa='+crypto.createHash('md5').update(data.query.login + data.query.password + Date.parse(new Date())).digest("hex")+';path=/;expires='+date.toGMTString()+';HttpOnly'});
             if (req.cookies.controls) {
                 console.log(req.cookies);
                 requests.checkSession(req.cookies.controls, data.query.start, data.query.end, res);
@@ -93,24 +79,21 @@ var http = require('http'),
                 res.end('0');
             }
         }
-        //   }
         else {
             next();
         }
     });
 
     app.use(function(req, res, next) {
-        // if (req.headers.origin === 'localhost:1111') {
         var data = url.parse(req.url, true);
         if (data.pathname === '/reg') {
             res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
             requests.reg(data.query.login, data.query.password, res);
         }
-        // }
         else {
             next();
         }
     });
 
-    app.use(connect.static('/Users/1/Documents/GitHub/yandex'));
+    app.use(connect.static('/Users/nvzc/Documents/GitHub/yandex'));
 http.createServer(app).listen(1111);
